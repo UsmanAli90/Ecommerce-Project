fbe511f3d1a0d93cec7f94c2a2e1b4c261eb7670
UsmanAli90 <usmanali04768@gmail.com>
2024-08-02 04:34:46 -0700

    Feedback

--- Ecommerce/app/models/item.rb
+++ Ecommerce/app/models/item.rb
@@ -1,14 +1,15 @@
 class Item < ApplicationRecord
-    include Ransackable
-    belongs_to :category
-    has_one_attached :image
-    has_many :order_items
-    has_many :orders, through: :order_items
-    has_many :item_sizes
-    has_many :sizes, through: :item_sizes
-    validates :name, presence: true
-    validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
-    
-    scope :with_image, -> { joins(image_attachment: :blob) }
-end
+  include Ransackable
 
+  belongs_to :category
+  has_one_attached :image
+  has_many :order_items
+  has_many :orders, through: :order_items
+  has_many :item_sizes
+  has_many :sizes, through: :item_sizes
+
+  validates :name, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
+  scope :with_image, -> { joins(image_attachment: :blob) }
+end
