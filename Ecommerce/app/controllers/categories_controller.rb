705a39829454fe4cf843acc7e8d00aef4c52be03
UsmanAli90 <usmanali04768@gmail.com>
2024-08-02 04:24:54 -0700

    Resolved

--- Ecommerce/app/controllers/categories_controller.rb
+++ Ecommerce/app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :authenticate_user!
+
   def index
     @category = Category.find(params[:id])
     @q = @category.items.ransack(params[:q])
--- Ecommerce/app/controllers/items_controller.rb
+++ Ecommerce/app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!
+
   def index
     @q = Item.ransack(params[:q])
     @item = @q.result(distinct: true)
