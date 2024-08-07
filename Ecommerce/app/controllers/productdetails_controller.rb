7b4cbb340549efb5ce6a357e1de0f4275a352740
UsmanAli90 <usmanali04768@gmail.com>
2024-08-06 23:37:07 -0700

    Product controller fixed

--- Ecommerce/app/controllers/productdetails_controller.rb
+++ Ecommerce/app/controllers/productdetails_controller.rb
@@ -1,7 +1,5 @@
 class ProductdetailsController < ApplicationController
   def index
-  end
-  def show
     @item = Item.find(params[:id])
   end
 end
--- Ecommerce/config/routes.rb
+++ Ecommerce/config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   root 'items#index'
   get 'categories/:id', to: 'categories#index', as: 'category'
   get 'order', to: 'orders#index'
-  get 'products/:id', to: 'productdetails#show', as: 'product_details'
+  get 'products/:id', to: 'productdetails#index', as: 'product_details'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
